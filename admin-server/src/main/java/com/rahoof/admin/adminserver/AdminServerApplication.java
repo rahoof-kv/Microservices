d73c9360f1f1532a5a234c1fa914f395e2c8b90f
rahoofkv <COme2me715#>
2018-08-04 11:04:48 +0400

    Admin server

---
+++ admin-server/src/main/java/com/rahoof/admin/adminserver/AdminServerApplication.java
@@ -0,0 +1,34 @@
+package com.rahoof.admin.adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableAdminServer
+@Configuration
+@SpringBootApplication
+public class AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+
+    @Configuration
+    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+            http.logout().logoutUrl("/logout");
+            http.csrf().disable();
+
+            http.authorizeRequests().antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll();
+            http.authorizeRequests().antMatchers("/**").authenticated();
+
+            http.httpBasic();
+        }
+    }
+}
